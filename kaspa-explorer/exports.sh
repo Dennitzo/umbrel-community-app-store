6b36a5de0769cf687d61596d17fa7544bf3eee76
Dennitzo <56736330+Dennitzo@users.noreply.github.com>
2026-01-24 17:46:19 +0100

    #Pull

--- kaspa-explorer/exports.sh
+++ kaspa-explorer/exports.sh
@@ -3,13 +3,6 @@ export APP_KASPA_EXPLORER_UI_ADDRESS="10.21.24.2"
 export APP_KASPA_REST_SERVER_ADDRESS="10.21.24.3"
 export APP_KASPA_SOCKET_SERVER_ADDRESS="10.21.24.4"
 
-# Shared kaspa-database connection (cross-compose via host gateway)
-export APP_KASPA_DB_ADDRESS="host.docker.internal"
-export APP_KASPA_DB_PORT="5432"
-export APP_KASPA_DB_NAME="kaspa"
-export APP_KASPA_DB_USER="kaspa"
-export APP_KASPA_DB_PASSWORD="dbpassword"
-
 # For other apps that might need to reach the Explorer services directly
 export APP_KASPA_EXPLORER_PORT="8080"
 export APP_KASPA_EXPLORER_API_URI="http://${APP_KASPA_REST_SERVER_ADDRESS}:8000"
